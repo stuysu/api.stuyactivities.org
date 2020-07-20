6322aa6de44fcd5048ed7b4397544f2bb00291e8
Abir Taheer <abir@taheer.me>
2020-07-20 07:00:07 -0400

    added more filteres to organization search

--- graphql/resolvers/Query/organizations.js
+++ graphql/resolvers/Query/organizations.js
@@ -1,24 +1,27 @@
 module.exports = async (root, args, context) => {
 	const {
-		with: { keyword, tags, commitmentLevels },
+		with: {
+			keyword,
+			tags,
+			commitmentLevels,
+			meetingDays,
+			meetingFrequency: { min, max }
+		},
 		limit,
 		offset
 	} = args;
 
 	const { models } = context;
+	const { Op } = models.Sequelize;
 
 	const filterParams = {
-		where: {},
+		where: {
+			active: true
+		},
 		include: [],
 		limit,
 		offset
 	};
-
-	if (keyword) {
-		filterParams.where.name = {
-			[models.Sequelize.Op.like]: `%${keyword.trim()}%`
-		};
-	}
 
 	if (Array.isArray(tags)) {
 		filterParams.include.push({
@@ -30,8 +33,56 @@ module.exports = async (root, args, context) => {
 		});
 	}
 
+	const charterInclude = {
+		model: models.charters,
+		where: {
+			[Op.and]: [
+				{
+					meetingFrequency: {
+						[Op.gte]: min,
+						[Op.lte]: max
+					}
+				}
+			]
+		},
+		required: true
+	};
+
+	if (keyword) {
+		filterParams.where[Op.or] = {
+			name: {
+				[Op.like]: `%${keyword}%`
+			},
+			url: {
+				[Op.like]: `%${keyword}%`
+			}
+		};
+
+		const fieldsToCheck = Object.keys(models.charters.rawAttributes);
+		fieldsToCheck.splice(fieldsToCheck.indexOf('createdAt'), 1);
+		fieldsToCheck.splice(fieldsToCheck.indexOf('updatedAt'), 1);
+
+		const charterFilter = {
+			[Op.or]: fieldsToCheck.map(field => ({
+				[field]: {
+					[Op.like]: `%${keyword}%`
+				}
+			}))
+		};
+
+		charterInclude.where[Op.and].push(charterFilter);
+	}
+
 	if (Array.isArray(commitmentLevels)) {
-		filterParams.where.commitmentLevel = commitmentLevels;
+		charterInclude.where[Op.and].push({
+			commitmentLevel: commitmentLevels
+		});
+	}
+
+	if (Array.isArray(meetingDays)) {
+		charterInclude.where[Op.and].push({
+			meetingDays
+		});
 	}
 
 	return models.organizations.findAll(filterParams);
