515827f61a4280abb5916d75fe7b06397cd0e5f7
Abir Taheer <abir@taheer.me>
2020-07-20 04:02:39 -0400

    made model for commitment

---
+++ database/migrations/20200720080228-create-org-commitments.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('orgCommitments', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			organizationId: {
+				type: Sequelize.INTEGER
+			},
+			level: {
+				type: Sequelize.ENUM('low', 'high', 'medium')
+			},
+			frequency: {
+				type: Sequelize.INTEGER
+			},
+			description: {
+				type: Sequelize.TEXT
+			},
+			days: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('orgCommitments');
+	}
+};
