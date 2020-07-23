8971cacfbc5b6640f53050a0cf0a76e39cb3a69e
Abir Taheer <abir@taheer.me>
2020-07-23 08:43:59 -0400

    prettified files

--- database/migrations/20200723105402-create-memberships.js
+++ database/migrations/20200723105402-create-memberships.js
@@ -1,36 +1,36 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('memberships', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      organizationId: {
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      role: {
-        type: Sequelize.STRING
-      },
-      adminPrivileges: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('memberships');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('memberships', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			organizationId: {
+				type: Sequelize.INTEGER
+			},
+			userId: {
+				type: Sequelize.INTEGER
+			},
+			role: {
+				type: Sequelize.STRING
+			},
+			adminPrivileges: {
+				type: Sequelize.BOOLEAN
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('memberships');
+	}
 };
--- database/migrations/20200723105419-create-membership-requests.js
+++ database/migrations/20200723105419-create-membership-requests.js
@@ -1,48 +1,48 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('membershipRequests', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      organizationId: {
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      role: {
-        type: Sequelize.STRING
-      },
-      adminPrivileges: {
-        type: Sequelize.BOOLEAN
-      },
-      userMessage: {
-        type: Sequelize.STRING
-      },
-      adminMessage: {
-        type: Sequelize.STRING
-      },
-      userApproval: {
-        type: Sequelize.BOOLEAN
-      },
-      adminApproval: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('membershipRequests');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('membershipRequests', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			organizationId: {
+				type: Sequelize.INTEGER
+			},
+			userId: {
+				type: Sequelize.INTEGER
+			},
+			role: {
+				type: Sequelize.STRING
+			},
+			adminPrivileges: {
+				type: Sequelize.BOOLEAN
+			},
+			userMessage: {
+				type: Sequelize.STRING
+			},
+			adminMessage: {
+				type: Sequelize.STRING
+			},
+			userApproval: {
+				type: Sequelize.BOOLEAN
+			},
+			adminApproval: {
+				type: Sequelize.BOOLEAN
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('membershipRequests');
+	}
 };
