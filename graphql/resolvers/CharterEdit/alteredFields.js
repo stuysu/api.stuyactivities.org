ab1bbd51998f85f5b7c22cba2ed907fe798d2898
Abir Taheer <abir@taheer.me>
2020-07-29 12:04:06 -0400

    fixed alteredFields resolver

--- graphql/resolvers/CharterEdit/alteredFields.js
+++ graphql/resolvers/CharterEdit/alteredFields.js
@@ -1,5 +1,9 @@
 const { EDITABLE_CHARTER_FIELDS } = require('./../../../constants');
 
 module.exports = charterEdit => {
-	return EDITABLE_CHARTER_FIELDS.filter(field => charterEdit[field] !== null);
+	return EDITABLE_CHARTER_FIELDS.filter(
+		field =>
+			typeof charterEdit[field] !== 'undefined' &&
+			charterEdit[field] !== null
+	);
 };
