57811ad85fc63b455be667a3a55a7bf643183fbd
Abir Taheer <abir@taheer.me>
2020-08-05 02:53:01 -0400

    fixed undefined reference in oAuths resolver

--- src/graphql/resolvers/User/oAuths.js
+++ src/graphql/resolvers/User/oAuths.js
@@ -1,13 +1,13 @@
 import { ForbiddenError } from 'apollo-server-express';
 
-export default async (user, args, { models, session }) => {
-	context.session.authenticationRequired(['oAuths']);
+export default async (user, args, { models: { oAuthIds }, session }) => {
+	session.authenticationRequired(['oAuths']);
 
-	if (context.session.userId !== user.id) {
+	if (session.userId !== user.id) {
 		throw new ForbiddenError(
 			'You are only allowed to see your own oAuth identities.'
 		);
 	}
 
-	return await models.oAuthIds.userIdLoader.load(session.userId);
+	return await oAuthIds.userIdLoader.load(session.userId);
 };
