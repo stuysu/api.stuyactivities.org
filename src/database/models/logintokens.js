02ad350865faa8bde43645a68c8ae9fd82672781
Abir Taheer <abir@taheer.me>
2020-08-05 01:27:07 -0400

    added model for login tokens

---
+++ src/database/models/logintokens.js
@@ -0,0 +1,32 @@
+import { Model } from 'sequelize';
+import findOneLoader from '../dataloaders/findOneLoader';
+import findManyLoader from '../dataloaders/findManyLoader';
+
+module.exports = (sequelize, DataTypes) => {
+	class loginTokens extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			loginTokens.belongsTo(models.users);
+		}
+
+		static tokenLoader = findOneLoader(loginTokens, 'token');
+		static idLoader = findOneLoader(loginTokens, 'id');
+		static userIdLoader = findManyLoader(loginTokens, 'userId');
+	}
+	loginTokens.init(
+		{
+			userId: DataTypes.INTEGER,
+			token: DataTypes.STRING
+		},
+		{
+			sequelize,
+			modelName: 'loginTokens'
+		}
+	);
+	return loginTokens;
+};
