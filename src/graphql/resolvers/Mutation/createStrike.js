086bd2f8feb0e3676d307f5b89def1a65cb4b7c9
ESGit3 <ethanshan706@gmail.com>
2020-08-22 18:15:42 -0400

    added email function for strikes

--- src/graphql/resolvers/Mutation/createStrike.js
+++ src/graphql/resolvers/Mutation/createStrike.js
@@ -1,18 +1,38 @@
+import emailRenderer from '../../../utils/emailRenderer';
+import { parse } from 'node-html-parser';
+import mailer from '../../../utils/mailer';
+
 export default async (parent, args, context) => {
 	const {
 		session,
-		models: { strikes }
+		models: { strikes, users }
 	} = context;
 
 	session.authenticationRequired([]);
 	await session.adminRoleRequired('Strikes', ['createStrike']);
 
-	let { organizationId, weight, reviewer, reason } = args;
+	let { organizationId, weight, reviewer, reason, email } = args;
+
+	const user = await users.emailLoader.load(email);
 
 	const strike = await strikes.create({
 		organizationId,
 		weight,
 		reviewer,
 		reason
+	});
+
+	const htmlEmail = emailRenderer.render('strike.html', {
+		user,
+		reason
+	});
+	const plainTextMail = parse(htmlEmail).structuredText;
+
+	await mailer.sendMail({
+		from: '"StuyActivities Mailer" <mailer@stuyactivities.org>',
+		to: user.email,
+		subject: `Strikes | StuyActivities`,
+		text: plainTextMail,
+		html: htmlEmail
 	});
 };
