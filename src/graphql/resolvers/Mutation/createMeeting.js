0410820dc95ec73d73940cd92ed36c1c7064d0ea
Abir Taheer <abir@taheer.me>
2020-08-27 04:18:14 -0400

    added mutation resolver for scheduling a new meeting

---
+++ src/graphql/resolvers/Mutation/createMeeting.js
@@ -0,0 +1,102 @@
+import { UserInputError } from 'apollo-server-express';
+import moment from 'moment-timezone';
+import emailRenderer from '../../../utils/emailRenderer';
+import { parse } from 'node-html-parser';
+import mailer from '../../../utils/mailer';
+
+export default async (
+	root,
+	{ orgId, orgUrl, title, description, start, end },
+	{ models: { organizations, meetings, users, memberships }, session }
+) => {
+	session.authenticationRequired(['createMeeting']);
+
+	if (!orgId && !orgUrl) {
+		throw new UserInputError(
+			'You need to provide an organization id or organization url in order to create a meeting',
+			{
+				invalidArgs: ['orgId', 'orgUrl']
+			}
+		);
+	}
+
+	let org;
+
+	if (orgId) {
+		org = await organizations.idLoader.load(orgId);
+	} else if (orgUrl) {
+		org = await organizations.urlLoader.load(orgUrl);
+	}
+
+	await session.orgAdminRequired(org.id);
+
+	if (!title) {
+		throw new UserInputError('A title must be provided for the meeting', {
+			invalidArgs: ['title']
+		});
+	}
+
+	const now = new Date();
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+
+	if (isNaN(startDate.getTime()) || startDate < now) {
+		throw new UserInputError('Start date is not valid', {
+			invalidArgs: ['start']
+		});
+	}
+
+	if (isNaN || endDate < startDate || endDate < now) {
+		throw new UserInputError('End date is not valid', {
+			invalidArgs: ['end']
+		});
+	}
+
+	const meeting = await meetings.create({
+		organizationId: org.id,
+		title,
+		description,
+		start: startDate,
+		end: endDate
+	});
+
+	const members = await users.findAll({
+		include: {
+			model: memberships,
+			where: {
+				organizationId: org.id
+			},
+			required: true
+		}
+	});
+	const formattedStart = moment(startDate)
+		.tz('America/New_York')
+		.format('dddd, MMMM Do YYYY, h:mm a');
+
+	const formattedEnd = moment(endDate)
+		.tz('America/New_York')
+		.format('dddd, MMMM Do YYYY, h:mm a');
+
+	for (let i = 0; i < members.length; i++) {
+		const member = members[i];
+
+		const htmlEmail = emailRenderer.render('strikeNotification.html', {
+			member,
+			org,
+			meeting,
+			formattedStart,
+			formattedEnd
+		});
+		const plainTextMail = parse(htmlEmail).structuredText;
+
+		await mailer.sendMail({
+			from: `"StuyActivities Mailer" <mailer@stuyactivities.org>`,
+			to: member.email,
+			subject: `${org.name} scheduled a meeting | StuyActivities`,
+			text: plainTextMail,
+			html: htmlEmail
+		});
+	}
+
+	return meeting;
+};
