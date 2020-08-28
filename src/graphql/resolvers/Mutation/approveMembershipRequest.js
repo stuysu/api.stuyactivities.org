bf1c9974c4b5f8ba860d1bbd017a17781981bec4
Restyled.io <commits@restyled.io>
2020-08-28 17:02:48 +0000

    Restyled by prettier

--- src/graphql/resolvers/Mutation/approveMembershipRequest.js
+++ src/graphql/resolvers/Mutation/approveMembershipRequest.js
@@ -1,52 +1,62 @@
-import {ApolloError, UserInputError, ForbiddenError} from "apollo-server-express"
-import isClubAdmin from "../../../utils/isClubAdmin"
-import sendEmail from "../../../utils/sendEmail"
+import {
+	ApolloError,
+	UserInputError,
+	ForbiddenError
+} from 'apollo-server-express';
+import isClubAdmin from '../../../utils/isClubAdmin';
+import sendEmail from '../../../utils/sendEmail';
 
 export default async (parent, args, context) => {
-	const {orgId, userId} = args
-	const {session, models: {memberships, membershipRequests}} = context
+	const { orgId, userId } = args;
+	const {
+		session,
+		models: { memberships, membershipRequests }
+	} = context;
 
 	if (!orgId || !userId) {
 		throw new UserInputError(
-			"The organization ID (orgId) and user ID (userId) are required to approve a membership request!",
-			{invalidArgs: ["orgId", "userId"]}
-		)
+			'The organization ID (orgId) and user ID (userId) are required to approve a membership request!',
+			{ invalidArgs: ['orgId', 'userId'] }
+		);
 	}
 	//see if user is an admin
 	if (!isClubAdmin(session.userId, orgId, memberships)) {
-		throw new ForbiddenError("You do not have the right to remove members from this club!")
+		throw new ForbiddenError(
+			'You do not have the right to remove members from this club!'
+		);
 	}
 
 	const membershipRequest = await membershipRequests.findOne({
 		where: {
-			organizationId: orgId, userId, userApproval: true
+			organizationId: orgId,
+			userId,
+			userApproval: true
 		}
-	})
+	});
 	if (!membershipRequest) {
 		throw new ApolloError(
-			"Could not find an incoming membership request with that userId and that orgId",
-			"REQUEST_NOT_FOUND"
-		)
+			'Could not find an incoming membership request with that userId and that orgId',
+			'REQUEST_NOT_FOUND'
+		);
 	}
 
 	const membership = await memberships.create({
 		userId,
 		organizationId: orgId,
 		role: membershipRequest.role,
-		adminPrivileges: membershipRequest.adminPrivileges,
-	})
+		adminPrivileges: membershipRequest.adminPrivileges
+	});
 
-
-	const user = await membershipRequest.getUser()
-	const organization = await membershipRequest.getOrganization()
+	const user = await membershipRequest.getUser();
+	const organization = await membershipRequest.getOrganization();
 	await sendEmail({
 		to: user.email,
-		subject: "You have been added to a club",
-		template: "memberAdded.html",
+		subject: 'You have been added to a club',
+		template: 'memberAdded.html',
 		variables: {
 			user,
 			organization
 		}
-	})
-	return membership
-}
+	});
+	return membership;
+};
--- src/utils/isClubAdmin.js
+++ src/utils/isClubAdmin.js
@@ -6,4 +6,4 @@ export default async (userId, organizationId, memberships) => {
 			organizationId
 		}
 	});
-}
+};
