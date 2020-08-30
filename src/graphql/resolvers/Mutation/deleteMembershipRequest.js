9730124b872dfc5de6a18ce824bf56d00cddf9d5
Abir Taheer <abir@taheer.me>
2020-08-30 14:43:51 -0400

    fixed resolver for delete membership request

--- src/graphql/resolvers/Mutation/deleteMembershipRequest.js
+++ src/graphql/resolvers/Mutation/deleteMembershipRequest.js
@@ -1,4 +1,4 @@
-import { ApolloError } from 'apollo-server-errors';
+import { ApolloError, ForbiddenError } from 'apollo-server-errors';
 
 export default async (
 	root,
@@ -16,9 +16,15 @@ export default async (
 		);
 	}
 
-	if (request.userId !== session.userId) {
-		throw new ApolloError(
-			'Only the person who created a request is allowed to delete it.'
+	const isAdmin = await session.orgAdminRequired(
+		request.organizationId,
+		['deleteMembershipRequest'],
+		true
+	);
+
+	if (request.userId !== session.userId && !isAdmin) {
+		throw new ForbiddenError(
+			'Only the person who created a request or an admin of the associated club are allowed to delete it.'
 		);
 	}
 
