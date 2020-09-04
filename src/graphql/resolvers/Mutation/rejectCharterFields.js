6ed13d6dff1dafe15ce2fc799abedf05f1fb9a29
Abir Taheer <abir@taheer.me>
2020-09-03 23:29:36 -0400

    fixed resolver for rejecting charter fields

--- src/graphql/resolvers/Mutation/rejectCharterFields.js
+++ src/graphql/resolvers/Mutation/rejectCharterFields.js
@@ -1,32 +1,38 @@
 import { EDITABLE_CHARTER_FIELDS } from '../../../constants';
 import { ApolloError, UserInputError } from 'apollo-server-express';
 
-export default async (root, { fields, charterEditId }, { models, session }) => {
+export default async (
+	root,
+	{ fields, charterEditId },
+	{ models: { charterEdits, charterApprovalMessages }, session }
+) => {
 	session.authenticationRequired('rejectCharterFields');
 	await session.adminRoleRequired('charters', ['rejectCharterFields']);
 
-	const charterEdit = await models.charterEdits.idLoader.load(charterEditId);
+	const charterEdit = await charterEdits.idLoader.load(charterEditId);
 
 	if (!charterEdit) {
 		throw new ApolloError(
 			'There is no charter edit with that id.',
-			'CHARTER_EDIT_NOT_FOUND'
+			'ID_NOT_FOUND'
 		);
 	}
 
 	// Remove duplicates and fields that don't exist
-	fields = [...new Set(fields)];
+	fields = Array.from(new Set(fields));
+
+	const alteredFields = charterEdit.getAlteredFields();
 
 	fields.forEach(field => {
-		if (!EDITABLE_CHARTER_FIELDS.includes(field)) {
+		if (!alteredFields.includes(field)) {
 			throw new UserInputError(
-				`One or more fields provided do not exist: ${field}`,
+				`One or more fields provided are not valid for this charter edit: ${field}`,
 				{ invalidArgs: ['fields'] }
 			);
 		}
 	});
 
-	await models.charterApprovalMessages.create({
+	await charterApprovalMessages.create({
 		organizationId: charterEdit.organizationId,
 		userId: session.userId,
 		message: `Rejected the following fields: ${fields.join(', ')}`,
@@ -34,5 +40,33 @@ export default async (root, { fields, charterEditId }, { models, session }) => {
 		seen: false
 	});
 
-	return await charterEdit.rejectFields(fields, session.userId);
+	let rejectedEdit;
+
+	if (fields.length === alteredFields.length) {
+		charterEdit.status = 'rejected';
+		charterEdit.reviewerId = session.userId;
+		await charterEdit.save();
+
+		rejectedEdit = charterEdit;
+	} else {
+		// move over the approved fields to a new charter edit
+		const newObj = {
+			organizationId: charterEdit.organizationId,
+			createdAt: charterEdit.createdAt,
+			submittingUserId: charterEdit.submittingUserId,
+			reviewerId: session.userId,
+			status: 'rejected'
+		};
+
+		fields.forEach(field => {
+			newObj[field] = charterEdit[field];
+			charterEdit[field] = null;
+		});
+
+		await charterEdit.save();
+
+		rejectedEdit = await charterEdits.create(newObj);
+	}
+
+	return rejectedEdit;
 };
