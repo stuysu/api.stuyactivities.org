5bd14fda17d369e9ce29ea7649a764d3cff04e6d
ESGit3 <ethanshan706@gmail.com>
2020-09-15 23:09:37 -0400

    strike mutation edits

--- src/graphql/resolvers/Mutation/createStrike.js
+++ src/graphql/resolvers/Mutation/createStrike.js
@@ -1,6 +1,7 @@
 import emailRenderer from '../../../utils/emailRenderer';
 import { parse } from 'node-html-parser';
 import mailer from '../../../utils/mailer';
+import { ApolloError, ForbiddenError } from 'apollo-server-express';
 
 export default async (parent, args, context) => {
 	const {
@@ -11,28 +12,41 @@ export default async (parent, args, context) => {
 	session.authenticationRequired([]);
 	await session.adminRoleRequired('Strikes', ['createStrike']);
 
-	let { organizationId, weight, reviewer, reason, email } = args;
-
-	const user = await users.emailLoader.load(email);
+	let { orgId, weight, reason, reviewerId } = args;
 
 	const strike = await strikes.create({
-		organizationId,
+		orgId,
 		weight,
-		reviewer,
+		reviewerId,
 		reason
 	});
 
-	const htmlEmail = emailRenderer.render('strike.html', {
-		user,
-		reason
-	});
-	const plainTextMail = parse(htmlEmail).structuredText;
-
-	await mailer.sendMail({
-		from: '"StuyActivities Mailer" <mailer@stuyactivities.org>',
-		to: user.email,
-		subject: `Strikes | StuyActivities`,
-		text: plainTextMail,
-		html: htmlEmail
-	});
+	// const user = await users.emailLoader.load(email);
+	//
+	// if (!user) {
+	// 	throw new ApolloError(
+	// 		'There is no user with that email address',
+	// 		'USER_NOT_FOUND'
+	// 	);
+	// }
+	//
+	// if (user.email && user.email.endsWith('@stuy.edu') && !user.isFaculty) {
+	// 	throw new ForbiddenError(
+	// 		'Only faculty are allowed to use the magic link sign in method at this time.'
+	// 	);
+	// }
+	//
+	// const htmlEmail = emailRenderer.render('strike.html', {
+	// 	user,
+	// 	reason
+	// });
+	// const plainTextMail = parse(htmlEmail).structuredText;
+	//
+	// await mailer.sendMail({
+	// 	from: '"StuyActivities Mailer" <mailer@stuyactivities.org>',
+	// 	to: user.email,
+	// 	subject: `Strikes | StuyActivities`,
+	// 	text: plainTextMail,
+	// 	html: htmlEmail
+	// });
 };
