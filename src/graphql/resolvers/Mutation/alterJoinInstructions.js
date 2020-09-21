8a0c2dd495eade436cee5c5743742bcf1f23ab40
Victor Veytsman <vityavv@gmail.com>
2020-09-21 17:18:03 -0400

    Add mutation for join instructions

---
+++ src/graphql/resolvers/Mutation/alterJoinInstructions.js
@@ -0,0 +1,45 @@
+import {
+	ApolloError,
+	ForbiddenError,
+	UserInputError
+} from 'apollo-server-express';
+
+export default async (parent, args, context) => {
+	// first steps, make sure they have sufficient permissions to make changes to the charter
+	const {
+		session,
+		models: { organizations, joinInstructions }
+	} = context;
+	const { orgId, instructions, buttonEnabled } = args;
+
+	const org = await organizations.idLoader.load(orgId);
+
+	if (!org) {
+		throw new ApolloError(
+			"There's no organization with that id",
+			'ID_NOT_FOUND'
+		);
+	}
+
+	await session.orgAdminRequired(orgId)
+
+	const currJoinInstructions = await joinInstructions.orgIdLoader.load(orgId);
+	if (currJoinInstructions) {
+		if (instructions) {
+			currJoinInstructions.instructions = instructions;
+		}
+		if (buttonEnabled !== undefined && buttonEnabled !== null) {
+			currJoinInstructions.buttonEnabled = buttonEnabled;
+		}
+		return await currJoinInstructions.save();
+	} else {
+		return await joinInstructions.create({
+			organizationId: orgId,
+			instructions,
+			buttonEnabled:
+				buttonEnabled !== undefined && buttonEnabled !== null
+					? buttonEnabled
+					: true
+		});
+	}
+};
