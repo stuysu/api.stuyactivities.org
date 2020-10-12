d464f06ce21f951ac75feff6479d7f56c2276203
Abir Taheer <abir@taheer.me>
2020-10-12 15:22:20 -0400

    stop sending out pre-loaded tags because they're incomplete

--- src/graphql/resolvers/Organization/tags.js
+++ src/graphql/resolvers/Organization/tags.js
@@ -1,9 +1,4 @@
 export default (org, args, { models }) => {
-	// If they were preloaded
-	if (org.tags) {
-		return org.tags;
-	}
-
 	// Otherwise we need to make a query
 	return models.tags.orgIdLoader.load(org.id);
 };
