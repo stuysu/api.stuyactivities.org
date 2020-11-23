5449e4f0fd3505142702c238c9a1432a400fb87f
Restyled.io <commits@restyled.io>
2020-11-23 23:39:38 +0000

    Restyled by prettier

--- src/database/migrations/20201123231443-create-background-access-tokens.js
+++ src/database/migrations/20201123231443-create-background-access-tokens.js
@@ -1,30 +1,30 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('backgroundAccessTokens', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      service: {
-        type: Sequelize.STRING
-      },
-      token: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('backgroundAccessTokens');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('backgroundAccessTokens', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			service: {
+				type: Sequelize.STRING
+			},
+			token: {
+				type: Sequelize.TEXT
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('backgroundAccessTokens');
+	}
 };
