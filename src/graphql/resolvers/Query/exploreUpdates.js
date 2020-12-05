8ae54bb3367494051e3fa406dce8f4443cef3a91
Victor Veytsman <vityavv@gmail.com>
2020-12-05 18:55:20 -0500

    Don't show posts older than two weeks on the explore page

--- src/graphql/resolvers/Query/exploreUpdates.js
+++ src/graphql/resolvers/Query/exploreUpdates.js
@@ -1,10 +1,14 @@
 export default (root, args, { session, models }) => {
 	session.authenticationRequired(['exploreUpdates']);
-
+	const twoWeeksAgo = new Date();
+	twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
 	return models.updates.findAll({
 		where: {
 			type: 'public',
-			approval: 'approved'
+			approval: 'approved',
+			createdAt: {
+				[models.Sequelize.Op.gt]: twoWeeksAgo
+			}
 		},
 		order: [['createdAt', 'desc']]
 	});
