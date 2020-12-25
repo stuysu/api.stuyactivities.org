f0dc40840df6b6c45da92fc13ba22ed205203447
Abir Taheer <abir@taheer.me>
2020-12-25 07:33:58 -0500

    added cdn forwarding for thumbnails

--- src/graphql/resolvers/CloudinaryResource/thumbnail.js
+++ src/graphql/resolvers/CloudinaryResource/thumbnail.js
@@ -1,10 +1,15 @@
 const cloudinary = require('cloudinary').v2;
 export default (resource, { width, height }) =>
-	cloudinary.url(resource.id, {
-		width,
-		height,
-		gravity: 'center',
-		crop: 'thumb',
-		radius: 'max',
-		secure: true
-	});
+	cloudinary
+		.url(resource.id, {
+			width,
+			height,
+			gravity: 'center',
+			crop: 'thumb',
+			radius: 'max',
+			secure: true
+		})
+		.replace(
+			'https://res.cloudinary.com/stuyactivities/',
+			'https://image-cdn.stuyactivities.org/'
+		);
