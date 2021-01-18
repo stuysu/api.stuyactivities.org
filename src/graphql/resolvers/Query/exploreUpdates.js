bcf8b4e5105e24c462592409ffe7304f7d306218
Victor Veytsman <vityavv@gmail.com>
2021-01-17 19:42:47 -0500

    Add offset (oops)

--- src/graphql/resolvers/Query/exploreUpdates.js
+++ src/graphql/resolvers/Query/exploreUpdates.js
@@ -1,14 +1,16 @@
-export default (root, { limit }, { session, models }) => {
+export default async (root, { limit, offset }, { session, models }) => {
 	//TODO REMOVE THIS
 	//IF YOU'RE READING THIS AND YOU'RE NOT VICTOR GO AHEAD AND SLAP HIM IN THE FACE FOR ME PLEASE
 	//session.authenticationRequired(['exploreUpdates']);
 
-	return models.updates.findAll({
+	const results = await models.updates.findAll({
 		where: {
 			type: 'public',
 			approval: 'approved'
 		},
-		order: [['createdAt', 'desc']],
-		limit
+		order: [['createdAt', 'desc']]
 	});
+
+	if (limit) limit = limit + offset;
+	return results.slice(offset, limit);
 };
