0cd36b8039639a721f579fc6653fe8a0f342862e
ESGit3 <ethanshan706@gmail.com>
2021-03-12 11:53:08 -0500

    changed strikeComments to comments

--- src/graphql/schema/Strike.js
+++ src/graphql/schema/Strike.js
@@ -3,7 +3,7 @@ import { gql } from 'apollo-server-express';
 export default gql`
 	type Strike {
 		id: Int!
-		strikeComments: [StrikeComment]
+		comments: [StrikeComment]
 		organization: Organization
 		organizationId: Int
 		weight: Int
