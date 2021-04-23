499a8ea9b06f874d1f2bad64de9cab5190e7777f
Abir Taheer <abir@taheer.me>
2021-04-23 16:56:39 -0400

    manually declared imports for query

--- src/graphql/schema/index.js
+++ src/graphql/schema/index.js
@@ -1,17 +1,59 @@
-import fs from 'fs';
-import path from 'path';
+import AdminRole from './AdminRole';
+import Charter from './Charter';
+import CharterApprovalMessage from './CharterApprovalMessage';
+import CharterEdit from './CharterEdit';
+import CloudinaryResource from './CloudinaryResource';
+import ClubFairResponse from './ClubFairResponse';
+import DateTime from './DateTime';
+import GoogleCalendar from './GoogleCalendar';
+import HelpRequest from './HelpRequest';
+import HelpRequestMessage from './HelpRequestMessage';
+import JoinInstructions from './JoinInstructions';
+import LinkPreview from './LinkPreview';
+import Meeting from './Meeting';
+import Membership from './Membership';
+import MembershipRequest from './MembershipRequest';
+import Mutation from './Mutation';
+import OAuthIdentity from './OAuthIdentity';
+import Organization from './Organization';
+import Query from './Query';
+import Strike from './Strike';
+import StrikeComment from './StrikeComment';
+import Tag from './Tag';
+import Update from './Update';
+import UpdateApprovalMessage from './UpdateApprovalMessage';
+import UpdatePic from './UpdatePic';
+import UpdateQuestion from './UpdateQuestion';
+import Upload from './Upload';
+import User from './User';
 
-const schemas = [];
-const thisFile = path.resolve(__filename);
-
-const files = fs.readdirSync(__dirname);
-
-files
-	.map(schema => path.resolve(__dirname, schema))
-	.filter(file => file !== thisFile)
-	.forEach(file => {
-		const schema = require(file);
-		schemas.push(schema.default || schema);
-	});
-
-export default schemas;
+export default [
+	AdminRole,
+	Charter,
+	CharterApprovalMessage,
+	CharterEdit,
+	CloudinaryResource,
+	ClubFairResponse,
+	DateTime,
+	GoogleCalendar,
+	HelpRequest,
+	HelpRequestMessage,
+	JoinInstructions,
+	LinkPreview,
+	Meeting,
+	Membership,
+	MembershipRequest,
+	Mutation,
+	OAuthIdentity,
+	Organization,
+	Query,
+	Strike,
+	StrikeComment,
+	Tag,
+	Update,
+	UpdateApprovalMessage,
+	UpdatePic,
+	UpdateQuestion,
+	Upload,
+	User
+];
