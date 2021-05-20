28f1933d2bc102048372ae31f573f9ab78fea8cd
Abir Taheer <abir@taheer.me>
2021-05-20 11:42:06 -0400

    added static methods for keyPair model

--- src/database/models/keypairs.js
+++ src/database/models/keypairs.js
@@ -1,5 +1,39 @@
 'use strict';
-const { Model } = require('sequelize');
+const { generateKeyPair, randomBytes } = require('crypto');
+const { Model, Op } = require('sequelize');
+
+function generateKeyPairPromise(passphrase, modulusLength = 2048) {
+	if (!passphrase) {
+		passphrase = randomBytes(16).toString('hex');
+	}
+
+	return new Promise(async (resolve, reject) => {
+		generateKeyPair(
+			'rsa',
+			{
+				modulusLength,
+				publicKeyEncoding: {
+					type: 'spki',
+					format: 'pem'
+				},
+				privateKeyEncoding: {
+					type: 'pkcs8',
+					format: 'pem',
+					cipher: 'aes-256-cbc',
+					passphrase
+				}
+			},
+			(err, publicKey, privateKey) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve({ publicKey, privateKey, passphrase });
+				}
+			}
+		);
+	});
+}
+
 module.exports = (sequelize, DataTypes) => {
 	class keyPairs extends Model {
 		/**
@@ -9,6 +43,53 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
+		}
+
+		// Keys should last a year at most
+		static maxAge = 1000 * 60 * 60 * 24 * 365;
+
+		// Stop signing with the current key if there's less than thirty days till expiration
+		static signingBuffer = 1000 * 60 * 60 * 24 * 30;
+
+		static async getSigningKey() {
+			const latestExpiration = new Date(Date.now() + this.signingBuffer);
+
+			let key = await keyPairs.findOne({
+				where: {
+					expiration: {
+						[Op.gt]: latestExpiration
+					}
+				}
+			});
+
+			if (!key) {
+				key = await this.getNewKeyPair();
+			}
+
+			return key;
+		}
+
+		static async getNewKeyPair() {
+			const expiration = new Date(Date.now() + this.maxAge);
+			const { publicKey, privateKey, passphrase } =
+				await generateKeyPairPromise();
+
+			return await keyPairs.create({
+				publicKey,
+				privateKey,
+				passphrase,
+				expiration
+			});
+		}
+
+		static async getValidKeyPairs() {
+			return await keyPairs.findAll({
+				where: {
+					expiration: {
+						[Op.gt]: new Date()
+					}
+				}
+			});
 		}
 	}
 	keyPairs.init(
