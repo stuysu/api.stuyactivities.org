f7442d4433f6237fc1ce5443e390366f723e454a
Abir Taheer <abir@taheer.me>
2021-05-22 13:21:14 -0400

    fixed issue with admins not being able to load membership requests

--- src/graphql/resolvers/Organization/membershipRequests.js
+++ src/graphql/resolvers/Organization/membershipRequests.js
@@ -3,12 +3,12 @@ import { ForbiddenError } from 'apollo-server-errors';
 export default async (
 	org,
 	args,
-	{ models, authenticationRequired, isOrgAdmin, hasAdminRoles }
+	{ models, authenticationRequired, isOrgAdmin, hasAdminRole }
 ) => {
 	authenticationRequired();
 
 	const orgAdmin = isOrgAdmin(org.id);
-	const isAdmin = hasAdminRoles('charters');
+	const isAdmin = hasAdminRole('charters');
 
 	if (!orgAdmin && !isAdmin) {
 		throw new ForbiddenError(
