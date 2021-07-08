082106f49c36d342c9c1df8f1f26aded1f5079f9
Abir Taheer <abir@taheer.me>
2021-07-08 16:03:17 -0400

    created room models

---
+++ src/database/models/rooms.js
@@ -0,0 +1,26 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class rooms extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	rooms.init(
+		{
+			name: DataTypes.STRING,
+			floor: DataTypes.STRING,
+			approvalRequired: DataTypes.BOOLEAN
+		},
+		{
+			sequelize,
+			modelName: 'rooms'
+		}
+	);
+	return rooms;
+};
