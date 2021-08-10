bfaf8d69ada6de76f680079539c2eb56d9f08815
Ian Graham Martinez <Ian.graham28@gmail.com>
2021-08-10 12:36:21 -0400

    fixed capitalization

--- src/graphql/schema/Mutation.js
+++ src/graphql/schema/Mutation.js
@@ -160,9 +160,9 @@ export default gql`
 
 		# --- Groups ---
 		createGroup(orgId: Int!, name: String!): Group
-		deleteGroup(GroupId: Int!): Boolean
+		deleteGroup(groupId: Int!): Boolean
 		createGroupMembership(groupId: Int!, userId: Int!): GroupMembership
-		deleteGroupMembership(GroupMembershipId: Int!): Boolean
+		deleteGroupMembership(groupMembershipId: Int!): Boolean
 
 		# --- Help Requests ---
 		createHelpRequest(
