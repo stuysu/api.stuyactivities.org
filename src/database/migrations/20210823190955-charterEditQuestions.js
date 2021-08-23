4197730ce9ddeeb2af92498a66d28d163b3bb605
Victor Veytsman <vityavv@gmail.com>
2021-08-23 18:49:40 -0400

    Try to fix failed deployment (more details inside)
    deploy failed because the charterEdits table isn't called
    charteredits... was fine on my machine using sqlite bc ig table names
    are case-insensitive in sqlite but they're not in postgres. Anyway this
    is all a wild guess so if you're reading this and I'm wrong feel free to
    point and laugh.

--- src/database/migrations/20210823190955-charterEditQuestions.js
+++ src/database/migrations/20210823190955-charterEditQuestions.js
@@ -3,13 +3,13 @@
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		queryInterface.addColumn(
-			'charteredits',
+			'charterEdits',
 			'socials',
 			Sequelize.DataTypes.STRING
 		);
 	},
 
 	down: async (queryInterface, Sequelize) => {
-		queryInterface.removeColumn('charteredits', 'socials');
+		queryInterface.removeColumn('charterEdits', 'socials');
 	}
 };
