2672dff8695a7b077e93feb3cf6db9a1dee22c6e
Restyled.io <commits@restyled.io>
2021-09-21 02:16:30 +0000

    Restyled by prettier

--- src/graphql/resolvers/Organization/memberships.js
+++ src/graphql/resolvers/Organization/memberships.js
@@ -1,6 +1,10 @@
 export default async (org, params, { models }) => {
 	if (!params.onlyLeaders) {
-		return models.memberships.orgIdLoader.load(org.id, {}, { order: [['adminPrivileges', 'DESC']] });
+		return models.memberships.orgIdLoader.load(
+			org.id,
+			{},
+			{ order: [['adminPrivileges', 'DESC']] }
+		);
 	}
 
 	return models.memberships.orgIdLoader.load(org.id, { adminPrivileges: 1 });
