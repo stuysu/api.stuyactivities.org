c80548c0d46e8cba5876eb482df8eeaec8674ea8
I-graham <54394607+I-graham@users.noreply.github.com>
2021-10-27 17:54:02 -0400

    Removed option to record $2 candy sales.

--- src/graphql/resolvers/Mutation/recordBoogramPurchase.js
+++ src/graphql/resolvers/Mutation/recordBoogramPurchase.js
@@ -1,9 +1,14 @@
 import uploadPicStream from '../../../utils/uploadPicStream';
+import { ForbiddenError } from 'apollo-server-errors';
 import { UserInputError } from 'apollo-server-express';
 import { randomBytes } from 'crypto';
 
 export default async (_, args, { adminRoleRequired, models: { boograms } }) => {
 	adminRoleRequired('records');
 
+	if (args.twoDollarCount !== 0) {
+		throw new ForbiddenError("$2 candies are no longer allowed.");
+	}
+	
 	return boograms.create(args) !== null;
 };
