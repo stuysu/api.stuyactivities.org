2ed91f1511b2a218a9b40588cd837bdcaa137f3f
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-10-16 19:53:53 -0400

    Add migration

---
+++ src/database/migrations/20221016234041-boograms-update.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('sales', 'purchaserOsis', {
+          type: Sequelize.DataTypes.INTEGER
+        }, { transaction: t }),
+        queryInterface.addColumn('sales', 'recorderId', {
+          type: Sequelize.DataTypes.INTEGER
+        }, { transaction: t })
+      ]);
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.removeColumn('sales', 'purchaserOsis', { transaction: t }),
+        queryInterface.removeColumn('sales', 'recorderId', { transaction: t })
+      ]);
+    });
+  }
+};
