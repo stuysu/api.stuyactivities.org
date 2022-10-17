366c263699ae88b4ac788ee942bac1dbbcd1b3d0
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-10-16 20:23:07 -0400

    Update sequelize db model

--- src/database/models/sales.js
+++ src/database/models/sales.js
@@ -15,7 +15,9 @@ module.exports = (sequelize, DataTypes) => {
 		{
 			userId: DataTypes.INTEGER,
 			itemId: DataTypes.INTEGER,
-			count: DataTypes.INTEGER
+			count: DataTypes.INTEGER,
+      purchaserOsis: DataTypes.INTEGER,
+      recorderId: DataTypes.INTEGER
 		},
 		{
 			sequelize,
