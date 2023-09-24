9bf8ccc826777504826c339e7d1cdf15ab2e4b08
David Chen <dchen07@protonmail.com>
2023-09-24 15:20:00 -0400

    Ensure default UNLOCK

---
+++ src/database/migrations/20230924191815-default-lock-enum.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+	queryInterface.bulkUpdate('organizations',  { locked: 'UNLOCK' });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+	 *
+	 *
+     */
+	// No schema changes to revert
+  }
+};
