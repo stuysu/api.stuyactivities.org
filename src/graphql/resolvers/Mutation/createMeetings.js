49fac0225beb9d9e6753f2fa1b26cb7f8c9709ad
Restyled.io <commits@restyled.io>
2023-12-24 20:51:23 +0000

    Restyled by prettier

--- src/graphql/resolvers/Mutation/createMeetings.js
+++ src/graphql/resolvers/Mutation/createMeetings.js
@@ -1,20 +1,27 @@
 import createMeeting from './createMeeting';
-import moment from "moment";
+import moment from 'moment';
 import { UserInputError } from 'apollo-server-express';
 
-export default async (
-	root,
-	args,
-	ctx
-) => {
+export default async (root, args, ctx) => {
 	ctx.adminRoleRequired('admin');
-	if (args.weeks < 1) throw new UserInputError("Must repeat for a positive number of weeks!")
+	if (args.weeks < 1)
+		throw new UserInputError('Must repeat for a positive number of weeks!');
 	return Array.from(Array(args.weeks).keys()).map(i => {
-		console.log(args.start, moment(args.start).add(7, 'days'), args.end, moment(args.end).add(7, 'days'), i);
-		return createMeeting(root, {
-			...args,
-			start: moment(args.start).add(7 * i, 'days'),
-			end: moment(args.end).add(7 * i, 'days'),
-		}, ctx);
-	})
+		console.log(
+			args.start,
+			moment(args.start).add(7, 'days'),
+			args.end,
+			moment(args.end).add(7, 'days'),
+			i
+		);
+		return createMeeting(
+			root,
+			{
+				...args,
+				start: moment(args.start).add(7 * i, 'days'),
+				end: moment(args.end).add(7 * i, 'days')
+			},
+			ctx
+		);
+	});
 };
