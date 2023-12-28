9ed67b90256c570af749642c9194ffbe92524daf
David Chen <dchen07@protonmail.com>
2023-12-27 22:07:38 -0500

    Remove comments, run prettier

--- src/database/migrations/20231228020436-isFaculty-forbid-null.js
+++ src/database/migrations/20231228020436-isFaculty-forbid-null.js
@@ -2,12 +2,6 @@
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		/**
-		 * Add altering commands here.
-		 *
-		 * Example:
-		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-		 */
 		await queryInterface.changeColumn('users', 'isFaculty', {
 			type: Sequelize.DataTypes.BOOLEAN,
 			allowNull: false
@@ -15,13 +9,6 @@ module.exports = {
 	},
 
 	down: async (queryInterface, Sequelize) => {
-		/**
-		 * Add reverting commands here.
-		 *
-		 *
-		 * Example:
-		 * await queryInterface.dropTable('users');
-		 */
 		await queryInterface.changeColumn('users', 'isFaculty', {
 			type: Sequelize.DataTypes.BOOLEAN,
 			allowNull: true
